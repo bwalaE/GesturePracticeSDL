6290c064ef3b3be4c55af96d07eab722b63325ef
sakhraE <websjack@sheridancollege.ca>
2021-05-12 15:02:22 -0400

    added basic stuff for positioning/fitting the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,24 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, standard IO, and strings
+//Using SDL and standard IO
 #include <SDL.h>
-#include <SDL_image.h>
 #include <stdio.h>
-#include <iostream>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+//screen properties, set in init()
+int SCREEN_WIDTH;
+int SCREEN_HEIGHT;
+int SCREEN_XPOS;
+int SCREEN_YPOS;
+bool display2 = false;
 
 //Starts up SDL and creates window
 bool init();
 
 //Loads media
 bool loadMedia();
+//fit and position screen
+void fitWindow(int w, int h, int x, int y);
 
 //Frees media and shuts down SDL
 void close();
-
-//Loads individual image
-SDL_Surface* loadSurface(std::string path);
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
@@ -29,106 +26,8 @@ SDL_Window* gWindow = NULL;
 //The surface contained by the window
 SDL_Surface* gScreenSurface = NULL;
 
-//Current displayed PNG image
-SDL_Surface* gPNGSurface = NULL;
-
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
-		{
-			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
-			success = false;
-		}
-		else
-		{
-			//Initialize PNG loading
-			int imgFlags = IMG_INIT_PNG;
-			if (!(IMG_Init(imgFlags) & imgFlags))
-			{
-				printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-				success = false;
-			}
-			else
-			{
-				//Get window surface
-				gScreenSurface = SDL_GetWindowSurface(gWindow);
-			}
-		}
-	}
-
-	return success;
-}
-
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load PNG surface
-	gPNGSurface = loadSurface("res/brogue.png");
-	if (gPNGSurface == NULL)
-	{
-		printf("Failed to load PNG image!\n");
-		success = false;
-	}
-
-	return success;
-}
-
-void close()
-{
-	//Free loaded image
-	SDL_FreeSurface(gPNGSurface);
-	gPNGSurface = NULL;
-
-	//Destroy window
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
-
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
-
-SDL_Surface* loadSurface(std::string path)
-{
-	//The final optimized image
-	SDL_Surface* optimizedSurface = NULL;
-
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-	if (loadedSurface == NULL)
-	{
-		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
-	}
-	else
-	{
-		//Convert surface to screen format
-		optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
-		if (optimizedSurface == NULL)
-		{
-			printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
-		}
-
-		//Get rid of old loaded surface
-		SDL_FreeSurface(loadedSurface);
-	}
-
-	return optimizedSurface;
-}
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
 
 int main(int argc, char* args[])
 {
@@ -165,8 +64,11 @@ int main(int argc, char* args[])
 					}
 				}
 
-				//Apply the PNG image
-				SDL_BlitSurface(gPNGSurface, NULL, gScreenSurface, NULL);
+				//Fill the surface white
+				SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 0x3E, 0x3E, 0x3E));
+
+				//Apply the image
+				SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
 
 				//Update the surface
 				SDL_UpdateWindowSurface(gWindow);
@@ -178,4 +80,79 @@ int main(int argc, char* args[])
 	close();
 
 	return 0;
+}
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Monitor 1 or 2?
+		if (display2 == false) { //monitor 1
+			fitWindow(900, 797, 636, 26);
+		}
+		else { //monitor 2
+
+		}
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		SDL_SetWindowPosition(gWindow, SCREEN_XPOS, SCREEN_YPOS);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP("res/bor.bmp");
+	if (gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void fitWindow(int w, int h, int x, int y) {
+	SCREEN_WIDTH = w;
+	SCREEN_HEIGHT = h;
+	SCREEN_XPOS = x;
+	SCREEN_YPOS = y;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
 }
